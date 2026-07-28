7bc9e1e48476f657689aa1d3dfcb68a4598b25b0
Nezumika <utokunnnnnnn13@gmail.com>
2026-07-28 21:23:56 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql2/promise");
+
+const app = express();
+const PORT = 3000;
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "UtoKun1313",
+  database: "kotodaman_site"
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "inquiry.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`サーバー起動：http://localhost:${PORT}`);
+});
+
+app.post("/inquiry", async (req, res) => {
+
+  const { name, comment } = req.body;
+
+  // 最新バージョンを取得
+  const [versionRows] = await db.query(
+    `
+    SELECT id
+    FROM versions
+    ORDER BY release_date DESC
+    LIMIT 1
+    `
+  );
+
+  const latestVersionId = versionRows[0].id;
+
+
+  // 問い合わせを保存
+  await db.query(
+    `
+    INSERT INTO inquiries
+    (name, comment, date, version_id)
+    VALUES (?, ?, NOW(), ?)
+    `,
+    [name, comment, latestVersionId]
+  );
+
+  res.send("保存しました！");
+});
+
+app.get("/version", async (req, res) => {
+
+  const [rows] = await db.query(
+    `
+    SELECT version
+    FROM versions
+    ORDER BY release_date DESC
+    LIMIT 1
+    `
+  );
+
+  res.json(rows[0]);
+});
