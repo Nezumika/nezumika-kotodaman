943806da423d1c6cf0159647ca98ce2a56a0bab3
Nezumika <utokunnnnnnn13@gmail.com>
2026-08-02 02:23:06 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const db = mysql.createPool({
 // 💡 CORS設定（OPTIONSリクエストを含むすべての通信を完全許可）
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    // 👈 DELETE を追加したよ！
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS'); 
     res.header('Access-Control-Allow-Headers', 'Content-Type, x-admin-password');
     
     // 事前確認（OPTIONS）には即座に 200 OK を返す
